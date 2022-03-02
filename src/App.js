dcfdd11bee5702ea2a2ad750673660b31050026f
unknown <omurbekmamytbekov545@gmail.com>
2022-03-02 13:47:15 +0600

    facebook clone

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import Header from './components/Header/Header';
+import LeftSide from './components/LeftSide/LeftSide';
+import CenterSide from './components/CenterSide/CenterSide';
+import RightSide from './components/RightSide/RightSite';
+
+function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className='flex space-x-7'>
+        <LeftSide />
+        <CenterSide />
+        <RightSide />
+      </div>
+    </div>
+  )
+}
+
+export default App
